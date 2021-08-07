dfecb98c272cdea8ca354dd385f8a5bbd52d26cb
guygreenleaf <yaweh23@gmail.com>
2021-08-07 13:52:31 -0700

    init auth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './styles/App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+    <header>
+      <h1>This is Amplify Auth</h1>
+    </header>
+    <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
 
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
