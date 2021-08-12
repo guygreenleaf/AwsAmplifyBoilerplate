9f6d383b2999adcff4552b14bbea922606b0a7bf
Guy <yaweh23@gmail.com>
2021-08-11 19:25:59 -0700

    repush to point to docker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 import PersonPinCircleIcon from '@material-ui/icons/PersonPinCircle';
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
+// eslint-disable-next-line import/no-webpack-loader-syntax!
 mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 function Map() {
